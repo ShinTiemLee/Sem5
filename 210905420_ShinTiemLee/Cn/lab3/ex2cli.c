b597ad92b62dd09c5dac0be1ebde8d816e2aab36
Shin Tiem Lee <96584919+ShinTiemLee@users.noreply.github.com>
2023-08-22 01:00:52 +0530

    Update ex2cli.c

--- 210905420_ShinTiemLee/Cn/lab3/ex2cli.c
+++ 210905420_ShinTiemLee/Cn/lab3/ex2cli.c
@@ -1,30 +1,34 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<fcntl.h>
-#include<sys/socket.h>
-#include<sys/types.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#include<unistd.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
 int main()
 {
-int sd;
-struct sockaddr_in address;
-sd=socket(AF_INET,SOCK_DGRAM,0);
-address.sin_family=AF_INET;
-address.sin_addr.s_addr=inet_addr("127.0.0.1");//**
-address.sin_port=htons(9704);
-int len=sizeof(address);
-char buf[5000];
-printf("Enter HTTP request:\n");
-scanf("%s",buf);
-sendto(sd,buf,sizeof(buf),0,(struct sockaddr *)&address, len);
-printf("Enter website:\n");
-scanf("%s",buf);
-sendto(sd,buf,sizeof(buf),0,(struct sockaddr *)&address, len);
+    int sd, n, result;
+    char buf[10000];
+    struct sockaddr_in address;
+    sd = socket(AF_INET, SOCK_STREAM, 0);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr("142.250.190.142");
+    address.sin_port = htons(80);
+    int len = sizeof(address);
+    result = connect(sd, (struct sockaddr *)&address, len);
+    if (result == -1)
+    {
+        perror("\nCLIENT ERROR");
+        exit(1);
+    }
+    char req[] = "GET /index.html HTTP/1.1\r\n\r\n";
 
-recvfrom(sd,buf,sizeof(buf),0,(struct sockaddr *)&address,&len);
-printf("the server output is:\n");
-puts(buf);
-return 0;
+    write(sd, req, strlen(req));
+
+    n = read(sd, buf, sizeof(buf));
+
+    printf("Received : %s", buf);
+    return 0;
 }
